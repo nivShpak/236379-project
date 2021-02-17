e1a340053513e04d023695819b6922d04bfff4e7
Idan Fischman <ifischman@apple.com>
2021-02-17 11:12:47 +0200

    some optimisations

--- main.cpp
+++ main.cpp
@@ -28,19 +28,61 @@ int longestRun(string s){
     return max_run;
 }
 
+bool hasRunLongerThan(string s, int x) {
+    int current_run=0;
+    int i = 0;
+    char curr_char = s[0];
+    char next_char;
+    while(i < s.size()) {
+        next_char = s[i];
+        if (next_char == curr_char)
+            current_run++;
+        else
+            current_run = 1;
+        if (current_run > x)
+            return true;
+        i++;
+        curr_char = next_char;
+    }
+    return false;
+}
+
+void printHistogram(int max, unordered_map<string, int> vectors_sizes) {
+    // create the histogram - it's not a full one if we check only the at most 2-length runs
+    int i = 0;
+    int hist[max + 1];
+    for (i = 0; i <= max; i++) {
+        hist[i] = 0;
+    }
+    for(auto iter = vectors_sizes.begin(); iter != vectors_sizes.end(); ++iter) {
+        auto cur = iter->second;
+        hist[cur]++;
+        //cout << "first is: " << iter->first << " second is: " << iter->second << endl;
+    }
+    cout << "sizes histogram:" << endl;
+    for (i = 0; i <= max; i++) {
+        if (hist[i] > 0)
+            cout << i << "    " << hist[i] << endl;
+    }
+
+}
+
 int main() {
     int max = 0;
     auto start = steady_clock::now();
     auto middle = steady_clock::now();
     unsigned int i, j=0;
     unordered_map<string, int> vectors_sizes;
-    int total_vectors = 1<<25;
+    int total_vectors = 1<<19;
     for (i=0 ; i < total_vectors; ++i) {
-        string s = bitset<25>(i).to_string();
+        string s = bitset<19>(i).to_string();
 
         if (s[0] == '1') {
             // it's enough to check ony vectors starting with '0'
             break;
+        }
+        if (!hasRunLongerThan(s, 3)) {
+            continue;
         }
         if (duration_cast<seconds>(steady_clock::now() - middle).count() > 3) {
             cout << duration_cast<seconds>(steady_clock::now() - start).count() << ": " << i << " of " << total_vectors << endl;
@@ -55,8 +97,7 @@ int main() {
         }
         reverse(s_reverse.begin(), s_reverse.end());
         // the vector with the most vectors inside it's 2-indel ball, it's longest gun will be of 2 at most
-        if (longestRun(s) > 2 ||
-            (vectors_sizes.find(s_reverse) != vectors_sizes.end()) ||
+        if ((vectors_sizes.find(s_reverse) != vectors_sizes.end()) ||
             (vectors_sizes.find(s_not) != vectors_sizes.end())) {
             continue;
         }
@@ -76,21 +117,8 @@ int main() {
     auto end = steady_clock::now();
     cout << "2-indel check of " << j << " vectors out of " << i*2 << " took: " << duration_cast<seconds>(end - start).count() << "s" << endl;
 
-    // create the histogram - it's not a full one if we check only the at most 2-length runs
-    int hist[max];
-    for (i = 0; i < max; i++) {
-        hist[i] = 0;
-    }
-    for(auto iter = vectors_sizes.begin(); iter != vectors_sizes.end(); ++iter) {
-        auto cur = iter->second;
-        hist[cur]++;
-        //cout << "first is: " << iter->first << " second is: " << iter->second << endl;
-    }
-    cout << "sizes histogram:" << endl;
-    for (i = 0; i < max; i++) {
-        if (hist[i] > 0)
-            cout << i << "    " << hist[i] << endl;
-    }
+    //printHistogram(max, vectors_sizes);
+
     return 0;
 }
 
