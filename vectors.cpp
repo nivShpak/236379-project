c6e55c4c7f6e5535d066c466098f46a6fbb18a93
Idan Fischman <ifischman@apple.com>
2021-02-21 01:05:05 +0200

    calcTwoInsertions performance improvment
    Avoid doing extra calculations when we know that insertion
    was already in the hash

--- vectors.cpp
+++ vectors.cpp
@@ -22,29 +22,42 @@ int vectors::twoBallSize() {
     return hash2.size();
 }
 
+void insert_j(int j, string s, unordered_set<string>& hash, char prev_j) {
+    string tmp;
+    if (prev_j != '0') {
+        tmp = s;
+        tmp.insert(j, "0");
+        hash.insert(tmp);
+    }
+
+    if (prev_j != '1') {
+        tmp = s;
+        tmp.insert(j, "1");
+        hash.insert(tmp);
+    }
+}
+
 void vectors::calcTwoInsertions(unordered_set<string>& hash, string vec){
     string tmp;
-    for(int i=0 ; i<this->size-1 ; ++i){
-        for (int j = i+1; j < this->size; ++j) {
+    char prev_i = 'x';
+    char prev_j = 'x';
+    for(int i=0 ; i < this->size-1 ; ++i) {
+        if (prev_i != '0') {
             tmp = vec;
-            tmp.insert(i,"0");
-            tmp.insert(j,"0");
-            hash.insert(tmp);
-
-            tmp = vec;
-            tmp.insert(i,"1");
-            tmp.insert(j,"0");
-            hash.insert(tmp);
-
-            tmp = vec;
-            tmp.insert(i,"0");
-            tmp.insert(j,"1");
-            hash.insert(tmp);
-
-            tmp = vec;
-            tmp.insert(i,"1");
-            tmp.insert(j,"1");
-            hash.insert(tmp);
+            tmp.insert(i, "0");
+            for (int j = i + 1; j < this->size; ++j) {
+                insert_j(j, tmp, hash, prev_j);
+                prev_j = tmp[j];
+            }
         }
+        if (prev_i != '1') {
+            tmp = vec;
+            tmp.insert(i, "1");
+            for (int j = i + 1; j < this->size; ++j) {
+                insert_j(j, tmp, hash, prev_j);
+                prev_j = tmp[j];
+            }
+        }
+        prev_i = vec[i];
     }
 }
